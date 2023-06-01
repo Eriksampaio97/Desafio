73315081f57cbaedc90aa475d00fcde6f3bab93c
Erik Sampaio <erik.sampaio@hotmail.com>
2023-06-01 07:59:08 -0300

    feat(project): first commit

---
+++ tiffany/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <>
+      <img src="../../assets/logo-tifany.png" alt="" />
+      <div>Header</div>
+    </>
+  );
+}
